705d002bbfd47383307735e74c9c3401f6c025b1
Joonas Hakkarainen <joonas.hakkarainen@hotmail.com>
2020-01-19 20:51:13 +0200

    Added comment to SequenceRange.java

--- src/main/java/fibonacci/SequenceRange.java
+++ src/main/java/fibonacci/SequenceRange.java
@@ -7,6 +7,7 @@ final class SequenceRange {
     private final int start;
     private final int length;
 
+    /* Constructor for fibonacci sequence range object */
     public SequenceRange(int start, int length) {
         this.start = start;
         this.length = length;
